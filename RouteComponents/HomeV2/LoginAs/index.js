aef29a5f8c11ba2177327a29e3936c46889d228a
titusefferian <kiritosato@gmail.com>
2021-06-12 23:23:55 +0700

    signin signout

--- RouteComponents/HomeV2/LoginAs/index.js
+++ RouteComponents/HomeV2/LoginAs/index.js
@@ -2,15 +2,18 @@ import { memo } from "react";
 import firebase from "firebase/app";
 import { useAuth } from "reactfire";
 import { useLoginBtsDispatch } from "../contexts/LoginBTS";
+import { useRouter } from "next/router";
 
 const useLogin = () => {
   const { handleOnCloseBTS } = useLoginBtsDispatch();
   const auth = useAuth();
+  const { reload } = useRouter();
   return {
     handleGoogle: async () => {
       try {
         await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
         handleOnCloseBTS();
+        reload();
       } catch (err) {
         alert(err);
       }
@@ -21,6 +24,7 @@ const useLogin = () => {
           new firebase.auth.OAuthProvider("apple.com")
         );
         handleOnCloseBTS();
+        reload();
       } catch (err) {
         alert(err);
       }
--- components/NavBar/Tooltip.js
+++ components/NavBar/Tooltip.js
@@ -1,21 +1,33 @@
-import { memo, useEffect, useRef, useState } from "react";
+import "firebase/auth";
+import { useRouter } from "next/router";
+import { memo } from "react";
+import { useAuth } from "reactfire";
 import { useTooltip } from "./contexts/TooltipContext";
 
 const Tooltip = () => {
   const showTooltip = useTooltip();
-
+  const auth = useAuth();
+  const { reload } = useRouter();
   return (
     <div
-      className={`${
-        showTooltip ? "opacity-100" : "opacity-0"
-      } ${showTooltip ? 'visible' : 'invisible'} transition-all ease-in-out duration-300 p-4 w-48 bg-white rounded shadow border absolute top-0 right-0 mt-8`}
+      className={`${showTooltip ? "opacity-100" : "opacity-0"} ${
+        showTooltip ? "visible" : "invisible"
+      } transition-all ease-in-out duration-300 p-4 w-48 bg-white rounded shadow border absolute top-0 right-0 mt-8`}
     >
       <ol>
         <li className="text-sm">
           <button>add your wishlist song</button>
         </li>
         <li className="text-sm mt-2">
-          <button>Logout</button>
+          <button
+            onClick={() => {
+              auth.signOut().then(() => {
+                reload();
+              });
+            }}
+          >
+            Logout
+          </button>
         </li>
       </ol>
     </div>
